a9a36670489dc2a13589649347beec087dad5e50
KiHoonShin <153797167+KiHoonShin@users.noreply.github.com>
2023-12-20 16:28:36 +0900

    23-12-20

--- level9/src/_09컬렉션프레임워크/_04Set이론.java
+++ level9/src/_09컬렉션프레임워크/_04Set이론.java
@@ -1,10 +1,24 @@
 package _09컬렉션프레임워크;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class _04Set이론 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Set<Integer> list1 = new HashSet<>();
+		list1.add(1);
+		list1.add(1);
+		list1.add(2);
+		list1.add(3);
+		
+		System.out.println(list1);
+		System.out.println(list1.size());
+		
+		
+		
+		
 	}
 
 }
