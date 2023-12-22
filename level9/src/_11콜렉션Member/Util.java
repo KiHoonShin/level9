24b921106ab9475fc4db6c34ae8d5edf70c71e02
KiHoonShin <153797167+KiHoonShin@users.noreply.github.com>
2023-12-22 10:39:06 +0900

    23-12-22

--- level9/src/_11콜렉션Member/Util.java
+++ level9/src/_11콜렉션Member/Util.java
@@ -6,4 +6,21 @@ public class Util {
 
 	static Scanner sc = new Scanner(System.in);
 	
+	private static Util instance = new Util();
+	public static Util getInstance(){
+		return instance;
+	}
+	
+	public int getValue(String msg, int start, int end) {
+		while(true) {
+			System.out.printf("%s [%d ~ %d] %n",msg,start,end);
+			int sel = sc.nextInt();
+			if(sel < start || sel > end) {
+				System.out.printf("%d ~ %d 범위 입력 %n" , start, end);
+				continue;
+			}
+			return sel;
+		}
+	}
+	
 }
