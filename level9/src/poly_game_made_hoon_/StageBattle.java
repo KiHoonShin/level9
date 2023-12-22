285af38b6d9d07c67626f5f2c14bd76631e72bdd
KiHoonShin <153797167+KiHoonShin@users.noreply.github.com>
2023-12-22 18:41:03 +0900

    Update StageBattle.java

--- level9/src/poly_game_made_hoon_/StageBattle.java
+++ level9/src/poly_game_made_hoon_/StageBattle.java
@@ -5,17 +5,33 @@ import java.util.Random;
 import java.util.Vector;
 
 public class StageBattle implements Stage {
-
+	UnitManager unitManager = new UnitManager();
+	ArrayList<Player> playerList = null;
+	ArrayList<Unit> monsterList = null;
+	
+	  
+	@Override
+	public void init() {
+		playerList = null;
+		playerList = unitManager.player_list;
+		monsterList = null;
+		monsterList = unitManager.mon_list;
+	}
+	
 	@Override
 	public boolean update() {
-		// TODO Auto-generated method stub
+		
 		return false;
 	}
 
-	@Override
-	public void init() {
-		// TODO Auto-generated method stub
-		
+	void print_character() {
+		System.out.println("======[BATTLE]======");
+		System.out.println("======[PLAYER]======");
+		for(int i = 0; i < playerList.size(); i++) {
+			System.out.println(playerList.get(i));
+		}
+		System.out.println("======[MONSTER]======");
 	}
+	
 	
 }
